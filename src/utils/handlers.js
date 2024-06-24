58aede27b1fe685e3b014b8cfe95431df46520d9
anghelo vincenzo manini <79824675+anghelomanini12@users.noreply.github.com>
2024-06-23 20:44:25 -0500

    Add files via upload

---
+++ src/utils/handlers.js
@@ -0,0 +1,12 @@
+const handleModalClose = (setIsModalShow) => {
+  setIsModalShow(false);
+};
+
+const handleModalOpen = (setIsModalShow) => {
+  setIsModalShow(true);
+};
+
+export {
+  handleModalClose,
+  handleModalOpen,
+}
